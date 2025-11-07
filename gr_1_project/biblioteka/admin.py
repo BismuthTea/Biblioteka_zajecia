2bb872fe0222d0ee05b054bd00fb7e8277a86523
BismuthTea <jkowalewski@st.swps.edu.pl>
2025-11-07 15:33:50 +0100

    zadania lab04 i 05

--- gr_1_project/biblioteka/admin.py
+++ gr_1_project/biblioteka/admin.py
@@ -2,8 +2,15 @@ from django.contrib import admin
 
 from .models import Genre, Author, Book, Person, Position
 
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'position', 'entry_date')
+    list_filter = ['position', 'entry_date']
+
+class StanowiskoAdmin(admin.ModelAdmin):
+    list_filter = ('name', 'description')
+
 admin.site.register(Genre)
 admin.site.register(Author)
 admin.site.register(Book)
-admin.site.register(Person)
-admin.site.register(Position)
+admin.site.register(Person, PersonAdmin,)
+admin.site.register(Position, StanowiskoAdmin)
